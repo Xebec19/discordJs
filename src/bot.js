9b5975d5103730b0517196ff6ec64ecf0476807c
xebec19 <rohant946@gmail.com>
2021-08-29 23:10:24 +0530

    setting up eslint and init

---
+++ src/bot.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+
+const client = new Client();
+
+// client.login(process.env.BOT_TOKEN);
+console.log(process.env.BOT_TOKEN);
